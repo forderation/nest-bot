41cb1cdbbff5476bc140d1ef70b3c80ee81cf445
forderation <kharisma.muzaki@gmail.com>
2020-09-07 00:32:25 +0700

    fix reminder

--- scheduler.py
+++ scheduler.py
@@ -11,6 +11,7 @@ class Scheduler:
         self.id_job_broadcast = 'nest-bot-reminders-broadcast'
         self.id_job_update_item = 'nest-bot-reminders-update-item'
         self.db = DBHelper()
+        self.db.build_connection()
         self.context = None
         self.is_on = False
         self.scheduler.add_job(self.__update_item_state, 'interval', hours=23, id=self.id_job_update_item,
@@ -20,8 +21,7 @@ class Scheduler:
     def turn_on_reminder(self, context_bot) -> bool:
         self.is_on = True
         self.context = context_bot
-        self.db.build_connection()
-        self.scheduler.add_job(self.__broadcast_remind, 'interval', seconds=15, id=self.id_job_broadcast,
+        self.scheduler.add_job(self.__broadcast_remind, 'interval', seconds=30, id=self.id_job_broadcast,
                                replace_existing=True)
         return True
 
@@ -32,7 +32,8 @@ class Scheduler:
             text_remind += "\n{} | {} | {} | {} | {} | {}".format(serial, product_name, jenis, product_state, update,
                                                                   employee_name)
         list_to_reminds = self.db.get_reminder_groups()
-        for id_g, chat_groups, title_groups in list_to_reminds:
+        print(list_to_reminds)
+        for id_g, chat_groups, title_groups, _, _ in list_to_reminds:
             self.context.bot.send_message(
                 chat_id=chat_groups,
                 text=text_remind
